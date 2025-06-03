80c12f083694e30029fcf72b90ee16ea619d2b96
MachineTheSlayer <dangodggw@gmail.com>
2025-06-04 00:27:37 +0400

    add fetch

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+
+import { getApiResourse } from '../../utils/network';
+
+const App = () => {
+  return (
+  <h1 className={styles.header}>Hello</h1>
+  ) 
+}
+
+export default App;
---
+++ src/utils/network.js
@@ -0,0 +1,30 @@
+
+const SWAPI_ROOT = 'https://swapi.py4e.com/api/';
+const SWAPI_PEOPLE = 'people';
+
+export const getApiResourse = async (url) => {
+    try {
+        const res = await fetch(url);
+
+        if (!res.ok) {
+            console.error('Could not fetch.', res.status);
+            return false;
+        }
+
+        return await res.json();
+    } catch (error) {
+        console.error('Could not fetch.', error.message);
+        return false;
+    }
+
+    /* fetch(url) 
+        .then(res => res.json())
+        .then(body => console.log(body))
+        .catch(error => console.log(error.message)) fetch через Promise */ 
+}
+
+
+(async () => {
+    const body = await getApiResourse(SWAPI_ROOT+SWAPI_PEOPLE);
+    console.log(body);
+})();
