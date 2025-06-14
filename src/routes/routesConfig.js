903ff2295433eddce45600e725bff9ddbc512b21
MachineTheSlayer <dangodggw@gmail.com>
2025-06-15 01:07:09 +0400

    add NotFoundPage

--- src/routes/routesConfig.js
+++ src/routes/routesConfig.js
@@ -1,14 +1,23 @@
 import HomePage from '@containers/HomePage';
 import PeoplePage from '@containers/PeoplePage';
+import NotFoundPage from '@containers/NotFoundPage';
 
 const routesConfig = [
     {
         path: '/',
         element: <HomePage />
     },
-     {
+    {
         path: '/people',
         element: <PeoplePage />
+    },
+    {
+        path: '/not-found',
+        element: <NotFoundPage />
+    },
+    {
+        path: '*',
+        element: <NotFoundPage />
     }
 ];
 
