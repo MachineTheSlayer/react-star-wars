b657b51bac7f30845890930abf7c51866a629d3b
MachineTheSlayer <dangodggw@gmail.com>
2025-07-08 01:06:53 +0400

    add react-context

---
+++ src/components/HomePage/ChooseSide/ChooseSide.jsx
@@ -0,0 +1,19 @@
+
+import { useTheme, THEME_LIGHT, THEME_DARK, THEME_NEITRAL} from "@context/ThemeProvider";
+
+import styles from "./ChooseSide.module.css";
+
+const ChooseSide = () => {
+    const isTheme = useTheme();
+
+    return (
+        <>
+            {isTheme.theme}
+            <button onClick={() => isTheme.change(THEME_LIGHT)}>Light</button>
+            <button onClick={() => isTheme.change(THEME_DARK)}>Dark</button>
+            <button onClick={() => isTheme.change(THEME_NEITRAL)}>Neitral</button>
+        </>
+    )
+}
+
+export default ChooseSide;
--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "@store/store";
-import '@styles/index.css';
+
+import ThemeProvider from "@context/ThemeProvider";
 import App from '@containers/App/App';
+import '@styles/index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <Provider store={store}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
