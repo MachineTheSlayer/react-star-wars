e5f67440247bdde2eb9e6f4ac45e676f8b832a45
MachineTheSlayer <dangodggw@gmail.com>
2025-07-16 23:58:36 +0400

    deploy

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "@store/store";
 
+import { REPO_NAME } from "@constants/repo";
+
 import ThemeProvider from "@context/ThemeProvider";
-import App from '@containers/App/App';
-import '@styles/index.css';
+import App from "@containers/App/App";
 
+import "@styles/index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const basename = process.env.NODE_ENV === "production" ? `/${REPO_NAME}/` : "/";
+const container = document.getElementById("root");
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Provider store={store}>
         <ThemeProvider>
           <App />
